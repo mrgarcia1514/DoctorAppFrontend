f4408ddcf5426a87e253fd6e80bb0bcae4b7b5e9
Allan G Garcia <allangabrielgarcia08@gmail.com>
2025-02-04 14:54:06 -0600

    agregar modulos

---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,8 @@
+export const environments = {
+
+  production: true,
+  apiUrl: 'http://localhost:5187/api/'
+
+
+}
+
---
+++ src/environments/environments.ts
@@ -0,0 +1,8 @@
+export const environments = {
+
+  production: false,
+  apiUrl: 'http://localhost:5187/api/'
+
+
+}
+
